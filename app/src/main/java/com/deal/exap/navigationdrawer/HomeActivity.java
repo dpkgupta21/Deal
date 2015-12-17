2ec571a63a28d1582ac32bc3a0b1ae01ab0ff23f
Mayur <mayur.tailor009@gmail.com>
2015-12-18 02:33:59 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/deal/exap/nearby/BuyCouponActivity.java
    #	app/src/main/res/layout/activity_redeem.xml

---
+++ app/src/main/java/com/deal/exap/navigationdrawer/HomeActivity.java
@@ -0,0 +1,188 @@
+package com.deal.exap.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.deal.exap.R;
+import com.deal.exap.alert.AlertFragment;
+import com.deal.exap.category.CategoriesFragment;
+import com.deal.exap.com.exap.sidemenu.ResideMenu;
+import com.deal.exap.com.exap.sidemenu.ResideMenuItem;
+import com.deal.exap.favorite.FavoriteFragment;
+import com.deal.exap.following.FollowingFragment;
+import com.deal.exap.interest.InterestFragment;
+import com.deal.exap.login.BaseActivity;
+import com.deal.exap.nearby.NearByFragment;
+import com.deal.exap.settings.SettingFragment;
+import com.deal.exap.wallet.WalletFragment;
+
+public class HomeActivity extends BaseActivity {
+
+    private ResideMenu resideMenu;
+    private HomeActivity mContext;
+    private ResideMenuItem itemAlert,itemNearby,itemWallet, itemInterest, itemFavorite, itemFollowing, itemCategory, itemSetting;
+    Boolean isopend=false;
+    View topView;
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        mContext = this;
+        init();
+        setUpMenu();
+
+    }
+
+    private void init(){
+        setHeader(getString(R.string.interest_screen_title));
+
+        setLeftClick(R.drawable.menu_btn);
+    }
+    private void setUpMenu() {
+
+        // attach to current activity;
+        resideMenu = new ResideMenu(this);
+        resideMenu.setBackgroundColor(getResources().getColor(R.color.nav_drw_bg));
+        resideMenu.attachToActivity(this);
+        resideMenu.setMenuListener(menuListener);
+
+        // create menu items;
+        itemAlert     = new ResideMenuItem(this, R.drawable.nav_bell_icon, getString(R.string.menu_alert));
+        itemNearby     = new ResideMenuItem(this, R.drawable.nav_nearby_icon, getString(R.string.menu_near_by));
+        itemWallet     = new ResideMenuItem(this, R.drawable.nav_wallet_icon, getString(R.string.menu_wallet));
+        itemInterest     = new ResideMenuItem(this, R.drawable.nav_interest_icon, getString(R.string.menu_interest));
+        itemFavorite     = new ResideMenuItem(this, R.drawable.nav_fav_icon, getString(R.string.menu_favorite));
+        itemFollowing     = new ResideMenuItem(this, R.drawable.nav_following_icon, getString(R.string.menu_following));
+        itemCategory     = new ResideMenuItem(this, R.drawable.nav_categories_icon, getString(R.string.menu_categories));
+        itemSetting     = new ResideMenuItem(this, R.drawable.nav_settings_icon, getString(R.string.menu_setting));
+
+        itemAlert.setOnClickListener(this);
+        itemNearby.setOnClickListener(this);
+        itemWallet.setOnClickListener(this);
+        itemInterest.setOnClickListener(this);
+        itemFavorite.setOnClickListener(this);
+        itemFollowing.setOnClickListener(this);
+        itemCategory.setOnClickListener(this);
+        itemSetting.setOnClickListener(this);
+
+        resideMenu.addMenuItem(itemAlert);
+        resideMenu.addMenuItem(itemNearby);
+        resideMenu.addMenuItem(itemWallet);
+        resideMenu.addMenuItem(itemInterest);
+        resideMenu.addMenuItem(itemFavorite);
+        resideMenu.addMenuItem(itemFollowing);
+        resideMenu.addMenuItem(itemCategory);
+        resideMenu.addMenuItem(itemSetting);
+
+        changeFragment(new InterestFragment());
+/*
+        findViewById(R.id.title_bar_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(isopend==false)
+                {
+                    resideMenu.openMenu();
+                    isopend=true;
+                }
+                else
+                {
+                    resideMenu.closeMenu();
+                    isopend=false;
+                }
+
+            }
+        });*/
+    }
+
+
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev)
+    {
+        //Toast.makeText(getApplicationContext(), "i m in Touch"+ev.getAction(),Toast.LENGTH_LONG).show();
+        return resideMenu.onInterceptTouchEvent(ev) || super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        resideMenu.closeMenu();
+
+        if (view == itemAlert){
+            changeFragment(new AlertFragment());
+            setHeader(getString(R.string.nearby_screen_title));
+        }else if (view == itemNearby){
+            changeFragment(new NearByFragment());
+            setHeader(getString(R.string.nearby_screen_title));
+        }else if (view == itemWallet){
+            changeFragment(new WalletFragment());
+            setHeader(getString(R.string.wallet_screen_title));
+        }else if (view == itemInterest){
+            changeFragment(new InterestFragment());
+            setHeader(getString(R.string.interest_screen_title));
+        }else if (view == itemFavorite){
+            changeFragment(new FavoriteFragment());
+            setHeader(getString(R.string.favorite_screen_title));
+        }else if (view == itemFollowing){
+            changeFragment(new FollowingFragment());
+            setHeader(getString(R.string.following_screen_title));
+        }else if (view == itemCategory){
+            changeFragment(new CategoriesFragment());
+            setHeader(getString(R.string.categories_title));
+        }else if (view == itemSetting){
+            changeFragment(new SettingFragment());
+            setHeader(getString(R.string.setting_screen_title));
+        }
+
+        switch (view.getId()){
+            case R.id.iv_back:
+                if(isopend==false)
+                {
+                    resideMenu.openMenu();
+                    isopend=true;
+                }
+                else
+                {
+                    resideMenu.closeMenu();
+                    isopend=false;
+                }
+                break;
+        }
+
+
+    }
+
+    private ResideMenu.OnMenuListener menuListener = new ResideMenu.OnMenuListener() {
+        @Override
+        public void openMenu() {
+            Toast.makeText(mContext, "Menu is opened!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void closeMenu() {
+            Toast.makeText(mContext, "Menu is closed!", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private void changeFragment(Fragment targetFragment){
+        resideMenu.clearIgnoredViewList();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.body_layout, targetFragment, "fragment")
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+    }
+
+    // What good method is to access resideMenu？
+    public ResideMenu getResideMenu(){
+        return resideMenu;
+    }
+}
