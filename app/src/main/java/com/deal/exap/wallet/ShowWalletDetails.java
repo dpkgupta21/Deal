a1be738be02d48bc5dd5c8572986a9a0d8e6780b
akshay1189 <akshay1189@gmail.com>
2016-01-22 00:38:36 +0530

    changes

---
+++ app/src/main/java/com/deal/exap/wallet/ShowWalletDetails.java
@@ -0,0 +1,240 @@
+package com.deal.exap.wallet;
+
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.deal.exap.R;
+import com.deal.exap.customerfeedback.CustomerFeedBackActivity;
+import com.deal.exap.following.FollowingPartnerDetails;
+import com.deal.exap.login.BaseActivity;
+import com.deal.exap.misc.ImageActivity;
+import com.deal.exap.model.DealDTO;
+import com.deal.exap.termscondition.TermsConditionActivity;
+import com.deal.exap.utility.Constant;
+import com.deal.exap.utility.TJPreferences;
+import com.deal.exap.utility.Utils;
+import com.deal.exap.volley.AppController;
+import com.deal.exap.volley.CustomJsonRequest;
+import com.google.gson.Gson;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.SimpleBitmapDisplayer;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShowWalletDetails extends BaseActivity {
+
+    //private GoogleMap mMap;
+    private DealDTO dealDTO;
+    private DisplayImageOptions options;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_coupon);
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);*/
+
+        init();
+    }
+
+
+    private void init() {
+
+        String id = getIntent().getStringExtra("id");
+        getDealDetails(id);
+
+        setViewVisibility(R.id.btn_buy,View.GONE);
+
+
+        options = new DisplayImageOptions.Builder()
+                .resetViewBeforeLoading(true)
+                .cacheOnDisk(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .considerExifParams(true)
+                .displayer(new SimpleBitmapDisplayer())
+                .showImageOnLoading(R.drawable.slide_img)
+                .showImageOnFail(R.drawable.slide_img)
+                .showImageForEmptyUri(R.drawable.slide_img)
+                .build();
+
+
+        setClick(R.id.iv_back);
+        setClick(R.id.thumbnail);
+        setClick(R.id.txt_terms_conditions);
+        setClick(R.id.txt_customer_reviews);
+        setClick(R.id.img_title);
+
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    /*@Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }*/
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        switch (view.getId()) {
+            case R.id.thumbnail:
+                i = new Intent(this, ImageActivity.class);
+                i.putExtra("image", dealDTO.getDeal_image());
+                startActivity(i);
+                break;
+//            case R.id.iv_chat:
+//                startActivity(new Intent(this, ChatActivity.class));
+//                break;
+            case R.id.txt_terms_conditions:
+                i = new Intent(this, TermsConditionActivity.class);
+                if (Utils.isArebic(this))
+                    i.putExtra("dealTerm", dealDTO.getTerm_ara());
+                else
+                    i.putExtra("dealTerm", dealDTO.getTerm_eng());
+                startActivity(i);
+
+                break;
+            case R.id.txt_customer_reviews:
+                i = new Intent(this, CustomerFeedBackActivity.class);
+                i.putExtra("dealId", dealDTO.getId());
+                startActivity(i);
+                break;
+
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.img_title:
+                i = new Intent(this, FollowingPartnerDetails.class);
+                i.putExtra("partnerId", dealDTO.getPartner_id());
+                startActivity(i);
+                break;
+
+        }
+    }
+
+
+
+
+    private void getDealDetails(String id) {
+
+
+        if (Utils.isOnline(this)) {
+            Map<String, String> params = new HashMap<>();
+            params.put("action", Constant.GET_DEAL_DETAIL);
+            params.put("lang", Utils.getSelectedLanguage(ShowWalletDetails.this));
+            params.put("lat", String.valueOf(TJPreferences.getLatitude(ShowWalletDetails.this.
+                    getApplicationContext())));
+            params.put("lng", String.valueOf(TJPreferences.getLongitude(this.
+                    getApplicationContext())));
+
+            params.put("deal_id", id);
+
+            final ProgressDialog pdialog = Utils.createProgeessDialog(this, null, false);
+            CustomJsonRequest postReq = new CustomJsonRequest(Request.Method.POST, Constant.SERVICE_BASE_URL, params,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                Utils.ShowLog(Constant.TAG, "got some response = " + response.toString());
+
+                                dealDTO = new Gson().fromJson(response.getJSONObject("deal").toString(), DealDTO.class);
+
+                                setData();
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            pdialog.dismiss();
+                        }
+                    }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    pdialog.dismiss();
+                    Utils.showExceptionDialog(ShowWalletDetails.this);
+                    //       CustomProgressDialog.hideProgressDialog();
+                }
+            });
+            AppController.getInstance().getRequestQueue().add(postReq);
+            postReq.setRetryPolicy(new DefaultRetryPolicy(
+                    30000, 0,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            pdialog.show();
+        } else {
+            Utils.showNoNetworkDialog(this);
+        }
+
+    }
+
+
+    private void setData() {
+
+        setTextViewText(R.id.txt_discount_rate, dealDTO.getDiscount() + "% off");
+        if (Utils.isArebic(this)) {
+            setTextViewText(R.id.txt_on_which, dealDTO.getName_ara());
+            setTextViewText(R.id.txt_details, dealDTO.getDetail_ara());
+        } else {
+            setTextViewText(R.id.txt_on_which, dealDTO.getName_eng());
+            setTextViewText(R.id.txt_details, dealDTO.getDetail_eng());
+        }
+
+
+        setTextViewText(R.id.txt_address, dealDTO.getLocation());
+        ((RatingBar) findViewById(R.id.rating_bar)).setRating(dealDTO.getRating());
+
+        setTextViewText(R.id.txt_review, dealDTO.getReview() + "");
+        setTextViewText(R.id.txt_end_date_val, dealDTO.getEnd_date());
+        setTextViewText(R.id.txt_redeemed_val, dealDTO.getRedeemed() + "");
+        setTextViewText(R.id.txt_distance_val, dealDTO.getDistance());
+        setTextViewText(R.id.txt_redeem_option, dealDTO.getRedeem_option());
+        setTextViewText(R.id.txt_discount, dealDTO.getDiscount() + "%");
+        setTextViewText(R.id.txt_store_price, dealDTO.getFinal_price());
+
+
+        ImageView imgThumnail = (ImageView) findViewById(R.id.thumbnail);
+        ImageView partner = (ImageView) findViewById(R.id.img_title);
+
+        ImageLoader.getInstance().displayImage(dealDTO.getDeal_image(), imgThumnail,
+                options);
+        ImageLoader.getInstance().displayImage(dealDTO.getPartner_logo(), partner,
+                options);
+
+    }
+
+
+}
